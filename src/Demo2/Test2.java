87859b1c5eb4e88fe992d5a91a67dcfb702becc3
maheshpolisetty456 <maheshpolisetty456@gmail.com>
2025-12-27 21:36:11 +0530

    doing check -in for code practice

--- src/Demo2/Test2.java
+++ src/Demo2/Test2.java
@@ -3,7 +3,7 @@ package Demo2;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("new file is used to push into the git");
+		System.out.println("doing 3rd change for git moficication");
 
 	}
 
